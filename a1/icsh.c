bb53bbca091d4ea9e3c70e3479e2a0fdf5223809
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-05-10 16:40:24 +0700

    doing ish

--- a1/icsh.c
+++ a1/icsh.c
@@ -159,6 +159,35 @@ void launch(process* p, pid_t p_groupid, int filein, fileout, int fileerr, int f
 
 }
 
+void put_job_in_fg(job* j, int c){
+
+	tcsetpgrp(shell_terminal, j->p_groupid); // Put job in fg
+
+	if (c){
+		tcgetattr(shell_terminal, TCSADRAIN, &j->termios);
+		if (kill(- j->p_groupid, SIGCONT) < 0){
+			perror("kill (SIGCONT)");
+		}
+
+
+		wait_for_job(j);
+
+		//Put shell in fg
+		tcsetpgrp(shell_terminal, shell_pgid);
+
+		tcgetattr(shell_terminal, &j->termios);
+		tcsetattr(shell_terminal, TCSADRAIN, &shell_termodes);
+	}
+}
+
+void put_job_in_bg(job* j, int c){
+	if (c){
+		if (kill(-j->p_groupid, SIGCONT) < 0){
+			perror("kill (SIGCONT");
+		}
+	}
+}
+
 void launch_jobs(job* j, int fg){
 
 	process* p;
@@ -230,6 +259,132 @@ void launch_jobs(job* j, int fg){
 	}
 
 
+}
+
+int mark_process_status(pid_t pid, int status){
+	job* j;
+	process* p;
+
+	if (pid > 0){
+
+		for (j = f_job; j; j = j->next){
+			for (p = j->f_proc; p; p = p->next){
+				if (p->pid == pid){
+
+					p->status = status;
+					if (WIFSTOPPED(status)){
+						p->stopornot = 1;
+					}
+					else{
+						p->complete = 1;
+						if (WIFSTOPPED(status)){
+							fprintf(stderr, "%d: Terminated by signal %d.\n", 
+								(int) pid, WTERMSIG(p->status));
+						}
+						return 0;
+					}
+
+					fprintf(stderr, "No child process %d.\n", pid);
+
+					return -1;
+
+				}
+
+			}
+		}
+	}
+	else if (pid == 0 || errno == ECHILD){
+		return -1;
+	}
+	else{
+		perror("waitpid");
+		return -1;
+	}
+
+}
+
+void update_status(){
+
+	int status;
+	pid_t pid;
+
+	do
+		pid = waitpid(WAIT_ANY, &status, WUNTRACED|WHOHANG);
+	while(!mark_process_status(pid, status));
+}
+
+
+void wait_for_job(job* j){
+
+	int status;
+	pid_t pid;
+
+	do
+		pid = waitpid(WAIT_ANY, &status, WUNTRACED);
+	while(!mark_process_status(pid,status) && !job_is_stopped(j) && !job_is_completed(j));
+}
+
+void format_job_info(job* j, const char* status){
+	fprintf(stderr, "%ld (%s): %s\n", (long)j->p_groupid, status, j->cmd);
+}
+
+void do_job_noti(){
+
+	job* j, jlast, jnext;
+	process *p;
+
+	update_status();
+
+	for (j = f_proc;j;j = jnext){
+		jnext = j->next;
+
+		// if completed, then delete from active jobs
+
+		if (job_is_completed(j)){
+
+			format_job_info(j, "completed");
+			if(jlast){
+				jlast->next = jnext;
+
+			}
+			else{
+				f_job = jnext;
+			}
+			free_job(j);
+		}
+		else if (job_is_stopped(j) && !j->isNotified){
+			format_job_info(j, "stopped");
+			j->isNotified = 1;
+			jlast = j;
+		}
+		else{
+			jlast = j;
+		}
+
+
+	}
+}
+
+void mark_job_as_running(job* j){
+	process* p;
+
+	for (p = j->f_proc; p;p = p->next){
+		p->stopornot = 0;
+	}
+
+	j->isNotified = 0;
+}
+
+void continue_job(job* j, int fg){
+
+	mark_job_as_running(j);
+
+	if (fg){
+		put_job_in_fg(j, 1);
+	}
+	else{
+		put_job_in_bg(j , 1);
+	}
 }
 
 int main(int argc, char *argv[]){
