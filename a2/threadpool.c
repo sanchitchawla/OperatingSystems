b89af1809fc1d12dae5a59c18b8e4da888df328e
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-05-22 22:36:09 +0700

    created threadpool

--- a2/threadpool.c
+++ a2/threadpool.c
@@ -13,8 +13,25 @@
 
 // _threadpool is the internal threadpool structure that is
 // cast to type "threadpool" before it given out to callers
+typedef struct queue
+{
+  void (*routine) (void*);
+  void *arg;
+  struct queue *next;
+} queue;
+
 typedef struct _threadpool_st {
    // you should fill in this structure with whatever you need
+  pthread_mutex_t lock;
+  pthread_cond_t empty;
+  pthread_cond_t not_empty;
+  pthread_t* threads;
+  int num_of_threads;
+  int size; 
+  int isShutDown;
+  int isNotAccept;
+  queue* head;
+  queue* tail;
 } _threadpool;
 
 
@@ -42,8 +59,39 @@ threadpool create_threadpool(int num_threads_in_pool) {
     return NULL;
   }
 
-  // add your code here to initialize the newly created threadpool
+  pool->threads = (pthread_t*)malloc(sizeof(pthread_t) * num_threads_in_pool);
 
+  if (!pool->threads){
+    fprintf(stderr, "Out of memory creating a new threadpool!\n");
+    return NULL;
+  }
+
+  pool->size        = 0;
+  pool->isNotAccept = 0;
+  pool->isShutDown  = 0;
+  pool->head        = NULL;
+  pool->tail        = NULL;
+  pool->num_of_threads = num_threads_in_pool;
+
+  if (pthread_cond_init(&pool->empty), NULL){
+    fprintf(stderr, "CV initiation error\n");
+    return NULL;
+  }
+  if (pthread_mutex_init(&pool->lock, NULL)){
+    fprintf(stderr, "Mutex error\n");
+  }
+  if (pthread_cond_init(&pool->not_empty), NULL){
+    fprintf(stderr, "CV initiation error\n");
+    return NULL;
+  }
+
+  for (int i = 0; i < num_threads_in_pool;i++){
+    if (pthread_create(&(pool->threads[i]), NULL, worker_thread, pool)){
+      fprintf(stderr, "Thread couldn't initialize\n");
+      return NULL;
+    }
+  }
+  
   return (threadpool) pool;
 }
 
